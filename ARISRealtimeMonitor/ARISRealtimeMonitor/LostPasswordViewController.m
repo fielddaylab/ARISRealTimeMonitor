1f998a2884380a89be6f4426a061c140cafea88d
m1sterc001guy <jgmoeller@wisc.edu>
2013-06-06 11:24:49 -0500

    Changed title of Lost Password to 'Lost Password'

--- ARISRealtimeMonitor/ARISRealtimeMonitor/LostPasswordViewController.m
+++ ARISRealtimeMonitor/ARISRealtimeMonitor/LostPasswordViewController.m
@@ -22,6 +22,7 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
+        self.title = @"Lost Password";
     }
     return self;
 }
